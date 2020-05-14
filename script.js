8b6b5c505135dc523adab9f71393448db65f2752
Oksana <opolishchuk89@outlook.com>
2020-05-14 19:22:26 +0300

    Tetris

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function myFunction() {
+        
+	var h1 = document.getElementsByTagName("h1")[0];
+    h1.innerHTML = "hello h3";
+}
+
+	//var button = document.getElementsByTagName("button")[0];
+    //button.click();
